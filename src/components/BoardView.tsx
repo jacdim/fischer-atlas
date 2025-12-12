9f3b04bedc56d60ff4a306e9df03dacdb32bddb7
jacdim <mxlimscrimali@gmail.com>
2025-12-12 22:20:16 +1100

    fix errors

--- src/components/BoardView.tsx
+++ src/components/BoardView.tsx
@@ -6,13 +6,11 @@ export default function BoardView({ fen }: { fen: string }) {
   return (
     <div className="w-full max-w-[400px] aspect-square">
       <Chessboard
-        id="BasicBoard"
-        position={fen}
-        boardOrientation="white"
-        // customDarkSquareStyle={{ backgroundColor: "#334155" }}
-        // customLightSquareStyle={{ backgroundColor: "#94a3b8" }}
-        arePiecesDraggable={false}
+        options={{
+          position: fen,
+          boardOrientation: "white"
+        }}
       />
     </div>
-  )
+  );
 }
